b4817f6ad6179051406e0274a1eaece53e788bb4
Your Name <you@example.com>
2024-04-15 12:13:29 +0200

    15/4/1

--- config/database.php
+++ config/database.php
@@ -19,7 +19,9 @@
 
 class Database{
 
-    public static function connect($host='localhost',$user='root',$pwd='',$db='motosjb'){
+    public static function connect($host='localhost',$user='root',$pwd='',$db='motos'){
+
+        
         $con = new mysqli($host,$user,$pwd,$db);
 
         if($con == false){
